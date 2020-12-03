58056c73a83342825afec26b3c971f9cedcb2e2f
Philipe Rocha <68014636+PhilipeRocha@users.noreply.github.com>
2020-12-02 23:46:12 -0300

    enviando calculadora produzida em vue.

---
+++ project/calculator/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from './main/calculator.vue'
+export default {
+    components: {
+        Calculator
+    }
+}
+
+</script>
+
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf")
+}
+
+* {
+    font-family: "RobotoMono", monospace;
+}
+
+body {
+    margin: 0
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #ffff;
+    background: linear-gradient(to right, rgb(195, 20, 50), rgb(36, 11, 54));
+}
+</style>
---
+++ project/calculator/src/components/button.vue
@@ -0,0 +1,56 @@
+<template>
+    <button class="button"
+    @click="$emit('onclick', label)"
+    :class="{ double, triple, operation }">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: { type: Boolean },
+        double: { type: Boolean },
+        triple: { type: Boolean }
+    }
+}
+
+</script>
+
+<style>
+:root {
+    --bg-button:  rgb(255, 225, 232);
+    --border-button: solid 1px rgb(42, 42, 42);
+}
+
+.button {
+    font-size: 1.4rem;
+    background-color: var(--bg-button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+}
+
+.button:active {
+    background-color:  rgb(235, 172, 182);
+}
+
+.button.double {
+    grid-column: span 2;
+}
+
+.button.triple {
+    grid-column: span 3;
+}
+
+.button.operation {
+    background-color:  rgb(167, 39, 60);
+    color: #fff;
+}
+
+.button.operation:active {
+    background-color: rgb(139, 23, 42);
+}
+</style>
